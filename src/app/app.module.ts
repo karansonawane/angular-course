29a5d70d57444b58ca3eaafeb50294c529eea0cd
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-11-20 17:02:45 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent, UserComponent
+    AppComponent, UserComponent, UsersComponent
   ],
   imports: [
     BrowserModule
