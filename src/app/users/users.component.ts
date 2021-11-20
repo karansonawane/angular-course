c8ae2c50c21f1e478b2c682dfadab40341ccf91b
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-11-20 20:26:48 +0530

    Update users.component.ts

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-users',
+  // selector: '[app-users]', //attribute selector
+  // selector: '.app-users',  //class selector
   templateUrl: './users.component.html',
   // template: `<app-user></app-user>`,
   styleUrls: ['./users.component.css']
