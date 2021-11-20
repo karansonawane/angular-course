13841b71a86b9b549423382d4a424b93b14d71e5
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-11-20 17:07:24 +0530

    Created component users with angular cli command

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  // template: `<app-user></app-user>`,
+  styleUrls: ['./users.component.css']
+  // styles: [
+  //   `
+  //   p{
+  //     color:red;
+  //   }
+
+  //   `
+  // ]
+})
+export class UsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
